f5a86cd797e8e5191745be0fb778303f05d4ae9f
Guillermo Torres <guigotoe@gmail.com>
2016-01-22 12:19:21 +0100

    putting threshold paramiters

--- Madrid/bin/netMetrics.py
+++ Madrid/bin/netMetrics.py
@@ -14,6 +14,7 @@
 __author__ = 'Guillermo G. Torres (PhD)'
 
 import re, sys, os, math
+import itertools as it
 import numpy as np
 import pandas as pd
 import cPickle as pickle
@@ -35,11 +36,11 @@ By Guillermo G. Torres (ggtorrese@unal.edu.co)
 - IKMB - Christian-Albrechts-Universitat zu Kiel"""
 
 parser = OptionParser(usage=usage,version="%prog 1.0")
-parser.add_option("-i","--input",type="string",action="store",dest="tensor",default='OTUp_SO_females',
+parser.add_option("-i","--input",type="string",action="store",dest="tensor",default='OTUp_SO_males',
                       help="Input file. Pandas dataframe (numpy based matrix)")
-parser.add_option("-a","--atrib",type="string",action="store",dest="atrib",default='SO_females',
+parser.add_option("-a","--atrib",type="string",action="store",dest="atrib",default='SO_males',
                       help="SOsin matrix")
-parser.add_option("-o","--out",type="string",action="store",dest="out",default='OTUp_females',
+parser.add_option("-o","--out",type="string",action="store",dest="out",default='OTUp_males',
                       help="SOsin matrix")
 parser.add_option("-S", action="store_true",dest="mothur",default=True,
                       help = "True if input is '.cons.tax.summary' from mothur - default False")
@@ -63,12 +64,30 @@ def main():
     j=0
     for i in files:
         tensor = pickle.load(open(i, 'r'))
-        so = pickle.load(open(so_files[j], 'r'))
+        #so = pickle.load(open(so_files[j], 'r'))
+        ft = {} # filtered tensor with all otus
+        st = {} # filtered tensor with filtered otus
+        q=0
         for k in tensor.keys():
-            tensor[k] = tensor[k][tensor[k]>0.1].fillna(0)  ## threshold to 0.1 for links removement
-            tensor[k].to_csv("%s_%s_t01.txt"%(out[j],k),sep='\t')
-        nets = netmetrics(tensor)
-        nets.wdegree().to_csv("%s_t01_wdegree.txt"%out[j],sep="\t")
+            #tensor[k] = tensor[k][tensor[k]>0.1].fillna(0)  ## threshold to 0.1 for links removement
+            df=tensor[k]
+            temp = []
+            permut = [x for x in it.combinations(df.index.values.tolist(),2)]   # All comparisons tuples (Oi,Oj)
+            m = pd.DataFrame(index=df.index.values.tolist(), columns=df.index.values.tolist()).fillna(0)  # Empty new matrix of network
+            for i in permut:temp.append([i[0],i[1],df.ix[i[0],i[1]]])
+            st[k] = temp
+            print temp
+            temp = pd.DataFrame(np.array(temp)).sort(2,ascending=False).reset_index().drop('index', axis=1)
+            print temp.ix[0:5,0:5]
+            print len(temp)
+            sys.exit('stop')
+            #temp.to_csv("%s_%s_best5p_net.txt"%(out[j],k),sep='\t',index=False)
+            #for i in range(int(0.05*len(permut))):m.ix[temp.ix[i,][0],temp.ix[i,][1]] = float(temp.ix[i,][2])
+            #m = m.add(m.transpose())
+            #m.to_csv("%s_%s_best5p.txt"%(out[j],k),sep='\t')
+            #ft[k] = m
+        nets = netmetrics(st)
+        nets.wdegree().to_csv("%s_best5p_wdegree_st.txt"%out[j],sep="\t")
         #nets.aveclustercoef().to_csv("%s_avclustcoef.txt"%out[j],sep="\t")
         #nets.eigvectc().to_csv("%s_eigvectc.txt"%out[j],sep="\t")
         #nets.betwcent().to_csv("%s_betwcent.txt"%out[j],sep="\t")
@@ -86,19 +105,40 @@ class netmetrics():
     def __init__(self,tensor):
         assert type(tensor) is dict
         self.nets = tensor
-        self.nodes = list(tensor[tensor.keys()[0]].columns.values)
-        self.indiv = tensor.keys()
+        if type(tensor[tensor.keys()[0]]) != list:
+            self.pd = True
+            self.nodes = list(tensor[tensor.keys()[0]].columns.values)
+            self.indiv = tensor.keys()
+        else:
+            self.pd = False
+            self.indiv = tensor.keys()
+
     def wdegree(self):
         '''
         It returns weighted degree dictionary of the nodes for each sample
         '''
-        deg=pd.DataFrame(np.nan,index=self.indiv,columns=self.nodes)
-        for k in self.indiv:
-            G = nx.from_numpy_matrix(self.nets[k].values)
-            G.edges(data=True)
-            wdegree = G.degree(weight='weight')
-            wd = [wdegree[node] for node in wdegree]
-            deg.loc[k] = wd
+        if self.pd :
+            deg=pd.DataFrame(np.nan,index=self.indiv,columns=self.nodes)
+            for k in self.indiv:
+                G = nx.from_numpy_matrix(self.nets[k].values)
+                G.edges(data=True)
+                wdegree = G.degree(weight='weight')
+                wd = [wdegree[node] for node in wdegree]
+                deg.loc[k] = wd
+        else:
+            deg = pd.DataFrame()
+            id = {}
+            for k in self.nets.keys():
+                G= nx.DiGraph()
+                G.add_weighted_edges_from(self.nets[k])
+                wdegree = G.degree(weight='weight')
+                #wd = node[wdegree[node] for node in wdegree]
+                wd={}
+                for node in wdegree:wd[node]=wdegree[node]
+                deg = deg.append(wd,ignore_index=True)
+                #print deg.index[-1]
+                id[deg.index[-1]]=k
+            deg.rename(index=id,inplace=True)
         return deg
     def aveclustercoef(self):
         '''
